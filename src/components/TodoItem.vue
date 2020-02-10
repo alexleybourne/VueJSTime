6936fa8ce2f45d633b22a46e80dbde649ccefb50
alexleybourne <alexleybourne1@gmail.com>
2020-02-10 15:07:59 +1000

    Delte functionality added

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,12 +1,22 @@
 <template>
-    <div>
-        <p>Todo Item</p>
+    <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+        <p>
+        <input type="checkbox" v-on:change="markComplete"/>
+        {{todo.title}}
+        <button class="del" @click="$emit('del-todo', todo.id)" >x</button>
+        </p>
     </div>
 </template>
 
 <script>
 export default {
-    name: "TodoItem"
+    name: "TodoItem",
+    props: ["todo"],
+    methods: {
+        markComplete() {
+            this.todo.completed = !this.todo.completed
+        }
+    }
 }
 </script>
 
