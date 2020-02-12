dc1353d18a02f6a7c5f9747fe01f502d9efde063
alexleybourne <alexleybourne1@gmail.com>
2020-02-12 13:51:53 +1000

    Function magic

---
+++ Vue-Introduction/app.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: '#app',
+    data: {
+        title: 'Becoming a Vue Pro'
+    },
+    methods: {
+        greet(){
+            return `Hi, My name is Alex`
+        }
+    }
+})
