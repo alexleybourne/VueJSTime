d8e26efe630703c92ba1e2d056aa61e7603dc2f5
alexleybourne <alexleybourne1@gmail.com>
2020-02-13 23:00:36 +1000

    Custom Search Box

--- myproject/src/main.js
+++ myproject/src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// Create Global Filter
+Vue.filter('snippet', val => {
+  if(!val || typeof(val) != 'string') return ''
+  val = val.slice(0,50)
+  return val + '...'
+})
+
 Vue.config.productionTip = false
 
 new Vue({
