950c66e419212f159a6b79f36a88e47a944f52a0
alexleybourne <alexleybourne1@gmail.com>
2020-02-15 18:45:07 +1100

    Watching the route magic

--- routing/src/router/index.js
+++ routing/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
+import ViewProfile from '../views/ViewProfile.vue'
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,11 @@ const routes = [
     path: '/about',
     name: 'About',
     component: About
+  },
+  {
+    path: '/profile/:user_id',
+    name: 'ViewProfile',
+    component: ViewProfile
   }
 ]
 
---
+++ routing/src/views/ViewProfile.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="view-profile">
+        <h2>Profile</h2>
+        <p>{{userId}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"ViewProfile",
+    data() {
+        return {
+            userId: this.$route.params.user_id
+        }
+    },
+    methods: {
+        updateId() {
+            this.userId = this.$route.params.user_id
+        }
+    },
+    watch: {
+        $route: 'updateId'
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
