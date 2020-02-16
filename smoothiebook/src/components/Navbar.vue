8e4cd2c20c6b8a116eccb4b7c0857d62cf24d1b9
alexleybourne <alexleybourne1@gmail.com>
2020-02-16 21:58:19 +1100

    Neat lil logo

--- smoothiebook/src/components/Navbar.vue
+++ smoothiebook/src/components/Navbar.vue
@@ -3,6 +3,7 @@
         <nav class="nav-extended blue darken-2">
             <div class="nav-content">
                 <router-link to="">
+                    <img class="nav-logo" src="../assets/SmoothieBookLogo.svg" alt="Smoothie Book Logo">
                     <span class="nav-title">SmoothieBook</span>
                 </router-link>
                 <a href="" class="btn-floating btn-large halfway-fab light-blue">
@@ -29,5 +30,13 @@ export default {
 <style scoped>
     .navbar nav {
         padding: 0 20px;
+    }
+
+    .nav-logo {
+        height: 50px;
+        width: auto;
+        margin: 0 10px 0 0;
+        position: relative;
+        top: 13px;
     }
 </style>
