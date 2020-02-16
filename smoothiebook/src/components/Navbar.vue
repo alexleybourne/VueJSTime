250ceedbf2c279a5b7a7d8ed599cafb206a11ec5
alexleybourne <alexleybourne1@gmail.com>
2020-02-16 21:33:54 +1100

    Setup project and nav created

---
+++ smoothiebook/src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="navbar">
+        <nav class="nav-extended blue darken-2">
+            <div class="nav-content">
+                <router-link to="">
+                    <span class="nav-title">SmoothieBook</span>
+                </router-link>
+                <a href="" class="btn-floating btn-large halfway-fab light-blue">
+                    <router-link to="">
+                        <i class="material-icons">add</i>
+                    </router-link>
+                </a>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Navbar",
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .navbar nav {
+        padding: 0 20px;
+    }
+</style>
