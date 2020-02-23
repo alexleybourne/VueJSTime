aa61ff3dc36144fdff4ba1395db54e77bdf29f65
alexleybourne <alexleybourne1@gmail.com>
2020-02-23 13:18:37 +1000

    Grabs smoothie to edit from firebase

--- smoothiebook/src/components/EditSmoothie.vue
+++ smoothiebook/src/components/EditSmoothie.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="edit-smoothie container">
-        <h2>Edit {{ this.$route.params.smoothie_slug}}</h2>
+    <div v-if="smoothie" class="edit-smoothie container">
+        <h2>Edit {{ smoothie.title }} Smoothie</h2>
     </div>
 </template>
 
@@ -10,9 +10,18 @@ export default {
     name:"EditSmoothie",
     data() {
         return {
-            
+            smoothie: null
         }
     },
+    created(){
+        let ref = db.collection('smoothies').where('slug', '==', this.$route.params.smoothie_slug)
+        ref.get().then(snapshot => {
+            snapshot.forEach(doc => {
+                this.smoothie = doc.data()
+                this.smoothie.id = doc.id
+            })
+        })
+    }
 }
 </script>
 
