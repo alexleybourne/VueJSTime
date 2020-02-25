d08ab6600e2f015dd441c8c37042e6a769cb838f
alexleybourne <alexleybourne1@gmail.com>
2020-02-25 22:59:10 +1000

    Welcome page made for chitchat

---
+++ chit-chat/src/components/Welcome.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="welcome container">
+    <div class="card">
+      <div class="card-content center-align">
+        <h2 class="blue-text">Welcome</h2>
+        <form @submit.prevent="enterChat">
+          <label for="name">Enter your name:</label>
+          <input type="text" name="name" v-model="name">
+          <button class="btn blue">Enter Chat</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Welcome',
+  data () {
+    return {
+      name: null
+    }
+  },
+  methods: {
+    enterChat(){
+      console.log(this.name)
+    }
+  }
+}
+</script>
+
+<style>
+
+.welcome {
+  max-width: 400px;
+  margin-top: 30vh;
+}
+
+.welcome h2 {
+  font-size: 2em;
+}
+
+.welcome button {
+  margin: 30px auto;
+}
+
+.welcome input {
+  text-align: center;
+}
+
+/* label focus color */
+.welcome input[type=text]:focus + label {
+    color: rgb(9, 164, 211) !important;
+}
+
+/* label underline focus color */
+.welcome input[type=text]:focus {
+    border-bottom: 1px solid rgb(9, 163, 210) !important;
+    box-shadow: 0 1px 0 0 rgb(5, 158, 205)!important;
+}
+
+.card {
+  border-radius: 20px !important;
+}
+
+.btn {
+  border-radius: 20px !important;
+}
+
+</style>
