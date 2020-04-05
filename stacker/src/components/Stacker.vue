4c5ce622f99e694931c936e0e9406e2fbe7bbb16
alexleybourne <alexleybourne1@gmail.com>
2020-04-05 16:25:15 +1000

    cleanup

---
+++ stacker/src/components/Stacker.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Hello</h1>
+</template>
+
+<script>
+export default {
+  name: 'Stacker',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
